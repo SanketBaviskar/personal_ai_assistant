0f4f59c2d181da6d8b756ae8cd57a6b94abf7e0c
Sanket Baviskar <sanketbaviskar01@gmail.com>
2025-12-02 17:22:39 -0700

    build error correcting

--- app/models/document.py
+++ app/models/document.py
@@ -6,7 +6,7 @@ from app.db.base_class import Base
 
 class Document(Base):
     id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
     filename = Column(String, nullable=False)
     file_size = Column(Integer, nullable=True)
     status = Column(String, default="pending") # pending, processing, completed, failed
